5726ee65c26123f325950f69d0a48193433f8a7e
Sean Cascketta <scascketta@gmail.com>
2014-05-11 21:08:15 -0500

    Use CDN hosted libraries

--- flaskcities/assets.py
+++ flaskcities/assets.py
@@ -2,14 +2,11 @@
 from flask.ext.assets import Bundle, Environment
 
 css = Bundle(
-    "libs/bootstrap3/css/bootstrap.min.css",
     "css/public/home.css",
     output="public/css/common.css"
 )
 
 js = Bundle(
-    "libs/jquery2/jquery-2.0.3.min.js",
-    "libs/bootstrap3/js/bootstrap.min.js",
     "js/utils.js", filters='jsmin',
     output="public/js/common.js"
 )
