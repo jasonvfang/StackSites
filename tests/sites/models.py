3918615563232f38a4fab60a4d22d0ecfa3b28ad
Sean Cascketta <scascketta@gmail.com>
2014-10-01 01:43:17 -0500

    Add basic Site model tests

---
+++ tests/sites/models.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import requests
+
+from stacksites.users.models import User
+from stacksites.sites.models import Site
+from stacksites.sites import utils
+
+
+class TestSites(object):
+
+    @pytest.yield_fixture
+    def user(request, app):
+        user = User.create(**{
+            'username': 'starlord',
+            'email': 'starlord@example.com',
+            'password': 'ihaveaplan'
+        })
+        yield user
+        user.delete_self()
+
+    @pytest.yield_fixture
+    def site(request, app, user):
+        site = Site.create(**{
+            'name': 'test',
+            'user': user
+        })
+        yield site
+        site.delete_site()
+
+    def test_new_site(self, site, user):
+        assert site in user.sites
+
+        files = site.get_files()
+        assert len(files) == 1
+        index_file = files[0]
+        assert 'index.html' == index_file['name']
+
+    def test_new_site_temp_file(self, user):
+        utils.update_temp_in_s3('xkcd', 'hello, world')
+        temp_file_url = utils.make_s3_path_for_temp('xkcd')
+        temp_file_data = requests.get(temp_file_url).content
+        assert temp_file_data == 'hello, world'
+
+        site = Site.create(**{
+            'name': 'test_with_temp_file',
+            'user': user,
+            'temp_file_id': 'xkcd'
+        })
+
+        assert site in user.sites
+
+        files = site.get_files()
+        index_file = files[0]
+        index_file_url = utils.make_s3_path(user.username, site.name, index_file['key'])
+        index_file_data = requests.get(index_file_url).content
+
+        assert index_file_data == 'hello, world'
+
+
