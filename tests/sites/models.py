891a66a674ef34567f1f13329e5f8edef28399e2
Sean Cascketta <scascketta@gmail.com>
2014-10-01 01:52:53 -0500

    Minor changes to site model tests

--- tests/sites/models.py
+++ tests/sites/models.py
@@ -22,10 +22,7 @@ class TestSites(object):
 
     @pytest.yield_fixture
     def site(request, app, user):
-        site = Site.create(**{
-            'name': 'test',
-            'user': user
-        })
+        site = Site.create(name='test', user=user)
         yield site
         site.delete_site()
 
@@ -38,24 +35,28 @@ class TestSites(object):
         assert 'index.html' == index_file['name']
 
     def test_new_site_temp_file(self, user):
+        # Create file in temp file S3 bucket
         utils.update_temp_in_s3('xkcd', 'hello, world')
+
+        # Make sure data in S3 matches
         temp_file_url = utils.make_s3_path_for_temp('xkcd')
         temp_file_data = requests.get(temp_file_url).content
         assert temp_file_data == 'hello, world'
 
-        site = Site.create(**{
-            'name': 'test_with_temp_file',
-            'user': user,
-            'temp_file_id': 'xkcd'
-        })
+        site = Site.create(
+            name='test_with_temp_file',
+            user=user,
+            temp_file_id='xkcd'
+        )
 
         assert site in user.sites
 
+        # Make sure data in new site on S3 matches
         files = site.get_files()
         index_file = files[0]
         index_file_url = utils.make_s3_path(user.username, site.name, index_file['key'])
         index_file_data = requests.get(index_file_url).content
 
-        assert index_file_data == 'hello, world'
+        assert index_file_data == temp_file_data
 
 
